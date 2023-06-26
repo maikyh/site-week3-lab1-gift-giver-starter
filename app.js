0acd846f6aeed474c89ea97c3426c5c8dbdaf0f4
Miguel Garza Carranza <maiky@meta.com>
2023-06-26 15:42:55 -0700

    Added the routes folder in which I implemented POST calls in the gift-exchange.js

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-const express = require("express");
-const app = express();
-module.exports = app;
+const express = require("express")
+const morgan = require("morgan")
+const app = express()
+const router = require('./routes/gift-exchange');
+
+app.use(morgan("tiny"))
+app.use(express.json())
+
+app.get("/", async (req, res) => {
+  res.json({ ping: "pong" })
+})
+
+app.use(`/gift-exchange`, router)
+
+module.exports = app
