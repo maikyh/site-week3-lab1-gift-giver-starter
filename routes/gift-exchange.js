377ad4f9f6e9f02312048fc0a20fcabec1de2b30
Miguel Garza Carranza <maiky@meta.com>
2023-06-26 16:13:06 -0700

    Implemented traditions and pair functions

--- routes/gift-exchange.js
+++ routes/gift-exchange.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const GiftExchange = require('../models/gift-exchange');
 
-router.post('/pairs', async (req, res) => {
-  res.status(200).json(req.body.names);
+router.post('/pairs', (req, res) => {
+  const names = req.body.names;
+
+  try {
+    const pairings = GiftExchange.pairs(names);
+    res.status(200).json(pairings);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
-router.post('/traditional', async (req, res) => {
-  res.status(200).json(req.body.names);
+router.post('/traditional', (req, res) => {
+  const names = req.body.names;
+
+  try {
+    const pairings = GiftExchange.traditional(names);
+    res.status(200).json(pairings);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 module.exports = router;
