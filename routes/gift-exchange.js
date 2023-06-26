1d054dfd3757c1387200735bd56bc5802681fdbf
Miguel Garza Carranza <maiky@meta.com>
2023-06-26 16:59:49 -0700

    Completed the Wire Up the Model and Routes

--- routes/gift-exchange.js
+++ routes/gift-exchange.js
@@ -1,26 +1,35 @@
 const express = require('express');
 const router = express.Router();
 const GiftExchange = require('../models/gift-exchange');
+const { BadRequestError } = require('../utils/errors');
 
-router.post('/pairs', (req, res) => {
+router.post('/pairs', (req, res, next) => {
   const names = req.body.names;
 
   try {
+    if (!names || !Array.isArray(names)) {
+      throw new BadRequestError('Invalid request body. "names" must be an array.');
+    }
+
     const pairings = GiftExchange.pairs(names);
     res.status(200).json(pairings);
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    next(error);
   }
 });
 
-router.post('/traditional', (req, res) => {
+router.post('/traditional', (req, res, next) => {
   const names = req.body.names;
 
   try {
+    if (!names || !Array.isArray(names)) {
+      throw new BadRequestError('Invalid request body. "names" must be an array.');
+    }
+
     const pairings = GiftExchange.traditional(names);
     res.status(200).json(pairings);
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    next(error);
   }
 });
 
